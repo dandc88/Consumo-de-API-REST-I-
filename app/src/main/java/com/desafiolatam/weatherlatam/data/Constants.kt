6ee9b957b4d3388150cc889c320be1b8e5754fbb
dandc88 <35500602+dandc88@users.noreply.github.com>
2024-08-07 19:29:32 -0400

    Update Constants.kt

--- app/src/main/java/com/desafiolatam/weatherlatam/data/Constants.kt
+++ app/src/main/java/com/desafiolatam/weatherlatam/data/Constants.kt
@@ -4,7 +4,7 @@ const val TAG = ">>>>>>"
 const val ITEM_ID = "ITEM_ID"
 const val CELSIUS = "CELSIUS"
 const val FAHRENHEIT = "FAHRENHEIT"
-const val OPEN_WEATHER_KEY = "000477936bfcd7a4b6c887a3a149a0a5"
+const val OPEN_WEATHER_KEY = ""
 const val OPEN_WEATHER_ENDPOINT = "https://api.openweathermap.org/data/2.5/"
 
 const val Unauthorized = "Unauthorized"
